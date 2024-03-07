2a74e45d852c9d623720887138ae079bc61e3243
Abigail Nunez <nuneza173@gmail.com>
2024-03-07 16:43:02 +0000

    Completed prep-javascript-operatos-and-expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+const number1 = 3;
+const number2 = 5;
+const product = number1 * number2;
+console.log('value of product:', product);
+console.log('typeof product:', typeof product);
+
+const charge = 35;
+const payment = 30;
+const amountRemaining = charge - payment;
+console.log('value of amountRemaining:', amountRemaining);
+console.log('typeof amountRemaining:', typeof amountRemaining);
+
+const tests = 25;
+const assignments = 45;
+const final = 38;
+const grade = (tests + assignments + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Abigail';
+const lastName = 'Nunez';
+let fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 9;
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const numberOfSoldiers = 115;
+const isSparta = numberOfSoldiers === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let nickname = (fullName += ' is the GOAT');
+console.log('value of nickname:', nickname);
+console.log('typeof nickname:', typeof nickname);
---
+++ prep-javascript-operators-and-expressions/prep-javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,44 @@
+# prep-javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What data type is returned by an arithmetic operation?
+  Numeric
+- What is string concatenation?
+  It is when you join strings together.
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+  Both numeric addition and string concatenation.
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+  Boolean
+- What does the `+=` "plus-equals" operator do?
+  It concatenates/adds a string onto the end of an existing string.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
